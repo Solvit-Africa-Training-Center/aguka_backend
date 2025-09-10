b63fe503af228660c2e4001fc34566b2eac57265
niyikiza20 <jbniyikiza20@gmail.com>
2025-09-10 16:12:57 +0200

    Authorisation check for user,group and contribution routes

--- src/routes/groupRoutes.ts
+++ src/routes/groupRoutes.ts
@@ -3,6 +3,8 @@ import { Router } from 'express';
 import { GroupController } from '../controllers/groupController';
 import { authMiddleware } from '../middlewares/authMiddleware';
 import { upload } from '../utils/upload';
+import { checkGroupMembership } from '../middlewares/groupMembershipMiddleware';
+import { checkRole } from '../middlewares/authMiddleware';
 
 const groupRouter = Router();
 
@@ -26,14 +28,20 @@ groupRouter.post(
  * @desc Get all groups
  * @access Protected
  */
-groupRouter.get('/groups', authMiddleware, GroupController.getAllGroups);
+groupRouter.get('/groups', authMiddleware, checkRole(['admin']), GroupController.getAllGroups);
 
 /**
  * @route GET /groups/:id
  * @desc Get a group by ID
  * @access Protected
  */
-groupRouter.get('/groups/:id', authMiddleware, GroupController.getGroupById);
+groupRouter.get(
+  '/groups/:id',
+  authMiddleware,
+  checkRole(['admin', 'president', 'treasurer', 'secretary', 'user']),
+  checkGroupMembership,
+  GroupController.getGroupById,
+);
 
 /**
  * @route PUT /groups/:id
@@ -43,6 +51,8 @@ groupRouter.get('/groups/:id', authMiddleware, GroupController.getGroupById);
 groupRouter.put(
   '/groups/:id',
   authMiddleware,
+  checkRole(['president', 'admin']),
+  checkGroupMembership,
   upload.fields([
     { name: 'profilePicture', maxCount: 1 },
     { name: 'agreementTerms', maxCount: 1 },
@@ -55,20 +65,36 @@ groupRouter.put(
  * @desc Delete a group
  * @access Protected
  */
-groupRouter.delete('/groups/:id', authMiddleware, GroupController.deleteGroup);
+groupRouter.delete(
+  '/groups/:groupId',
+  authMiddleware,
+  checkRole(['admin']),
+  GroupController.deleteGroup,
+);
 
 /**
  * @route POST /groups/join
  * @desc Join a group using group code
  * @access Protected
  */
-groupRouter.post('/groups/join', authMiddleware, GroupController.joinGroup);
+groupRouter.post(
+  '/groups/join',
+  authMiddleware,
+  checkRole(['admin', 'user']),
+  GroupController.joinGroup,
+);
 
 /**
  * @route GET /groups/:groupId/members
  * @desc Get all members of a group
  * @access Protected
  */
-groupRouter.get('/groups/:groupId/members', authMiddleware, GroupController.getGroupMembers);
+groupRouter.get(
+  '/groups/:groupId/members',
+  authMiddleware,
+  checkRole(['admin', 'president', 'treasurer', 'secretary']),
+  checkGroupMembership,
+  GroupController.getGroupMembers,
+);
 
 export default groupRouter;
